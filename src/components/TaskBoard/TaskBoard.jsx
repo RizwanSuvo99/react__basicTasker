ab1d2c54fb6b12a54248162b9001880332e0f056
RizwanSuvo99 <rizwanshuvo@gmail.com>
2024-01-26 12:51:19 +0600

    "Add Task functionality added."

--- src/components/TaskBoard/TaskBoard.jsx
+++ src/components/TaskBoard/TaskBoard.jsx
@@ -1,7 +1,8 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import SearchTask from "./SearchTask/SearchTask";
 import TaskActions from "./TaskActions.jsx/TaskActions";
 import TaskLists from "./TaskLists/TaskLists";
+import AddTaskModal from "./AddTaskModal/AddTaskModal";
 
 const TaskBoard = () => {
   const defaultTask = {
@@ -14,18 +15,22 @@ const TaskBoard = () => {
     isFavourite: true,
   };
   const [tasks, setTasks] = useState([defaultTask]);
-  const handleAddTask = () => {
-    console.log("New task added....");
+  const [showAddModal, setShowAddModal] = useState(false);
+
+  const handleAddTask = (newTask) => {
+    setTasks([...tasks, newTask]);
+    setShowAddModal(false);
   };
 
   return (
     <section className="mb-20" id="tasks">
+      {showAddModal && <AddTaskModal onSave={handleAddTask} />}
       <div className="container">
         <div className="p-2 flex justify-end">
           <SearchTask />
         </div>
         <div className="rounded-xl border border-[rgba(206,206,206,0.12)] bg-[#1D212B] px-6 py-8 md:px-9 md:py-16">
-          <TaskActions onAddClick={handleAddTask} />
+          <TaskActions onAddClick={() => setShowAddModal(true)} />
           <TaskLists tasks={tasks} />
         </div>
       </div>
